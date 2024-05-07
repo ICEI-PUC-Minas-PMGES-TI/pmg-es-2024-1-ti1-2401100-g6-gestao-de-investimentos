5a63cc02aba056413243309fb443dcfc4d950dda
MateusRodrigu <164748740+MateusRodrigu@users.noreply.github.com>
2024-05-07 19:09:50 -0300

    reestuturação

--- codigo/feed.js
+++ codigo/feed.js
@@ -1,10 +1,52 @@
+function lerNoticias() {
+    let strdados = localStorage.getItem('db');
+    let objDados = {};
+    if (strdados) {
+        objDados = JSON.parse(strdados);
+    } else {
+        objDados = {
+            Noticias: [{
+                titulo: "noticias teste",
+                Resumo: "esse e um teste de noticias",
+                link: "https://www.cnnbrasil.com.br/economia/negocios/brasil-importa-energia-do-uruguai-para-garantir-fornecimento-ao-rs/",
+                Image: "images/sua-imagem.jpg", // Substitua 'sua-imagem.jpg' pelo nome correto do arquivo da sua imagem
+            }],
+        };
+    }
+    return objDados;
+}
+
+function salvaNoticias(objDados) {
+    localStorage.setItem('db', JSON.stringify(objDados));
+}
+
+function incluirNoticias() {
+    let objDados = lerNoticias();
+    let strtitulo = document.getElementById('campoTitulo').value;
+    let strImage = document.getElementById('campoImg').value;
+    let strResumo = document.getElementById('campoResumo').value;
+    let strlink = document.getElementById('campoLink').value;
+    let NovaNoticia = {
+        titulo: strtitulo,
+        Resumo: strResumo,
+        link: strlink,
+        Image: strImage,
+    };
+    objDados.Noticias.push(NovaNoticia);
+    salvaNoticias(objDados);
+}
+
 function imprimirNoticias() {
     let tela = document.getElementById('noticias');
     let strHtml = '';
     let objDados = lerNoticias();
-    for (let i = 0; i < objDados.Noticias.length; i++) {
+    for (let i = objDados.Noticias.length-1 ; i >= 0; i--) {
         strHtml += '<div class="card" style="width: 20rem;"><h2>' + objDados.Noticias[i].titulo + '</h2><img src="' + objDados.Noticias[i].link + '" class="card-img-top" alt="..."><div class="card-body"><p class="card-text">' + objDados.Noticias[i].Resumo + '</p></div></div>';
     }
     tela.innerHTML = strHtml;
 }
-window.onload = imprimirNoticias
+
+window.onload = imprimirNoticias()
+
+
+
