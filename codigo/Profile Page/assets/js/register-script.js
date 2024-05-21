6a302fb2414471570422de29a9e99c7c1ba8b266
Lucas Ferreira <ljlferreira@sga.pucminas.br>
2024-05-21 18:28:22 -0300

    Máscara Telefone JS

--- codigo/Profile Page/assets/js/register-script.js
+++ codigo/Profile Page/assets/js/register-script.js
@@ -8,12 +8,20 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     const confirmPassword = document.getElementById('confirmPassword').value;
     const message = document.getElementById('message');
 
+    document.addEventListener('DOMContentLoaded', function() {
+        var telefone = document.getElementById('tel');
+    
+        telefone.addEventListener('input', function(e) {
+            var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+            e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+        });
+    });
+
     if (password !== confirmPassword) {
         message.textContent = 'As senhas não coincidem!';
         return;
     }
 
-    
     localStorage.setItem('username', username);
     localStorage.setItem('email', email);
     localStorage.setItem('tel', tel);
