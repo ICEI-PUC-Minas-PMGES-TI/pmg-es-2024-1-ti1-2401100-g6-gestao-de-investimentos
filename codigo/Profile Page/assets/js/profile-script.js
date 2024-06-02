d93dc6aa0672d7bc03b9ee29f1f8e863b6d68229
LucasFerreira <164096434+Lucas-Ferreira11@users.noreply.github.com>
2024-06-02 11:18:12 -0300

    Data de criação
    XD

--- codigo/Profile Page/assets/js/profile-script.js
+++ codigo/Profile Page/assets/js/profile-script.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const username = localStorage.getItem('username');
     const email = localStorage.getItem('email');
     const tel = localStorage.getItem('tel');
+    var today = new Date();
+            var day = String(today.getDate()).padStart(2, '0');
+            var month = String(today.getMonth() + 1).padStart(2, '0');
+            var year = today.getFullYear();
+            var formattedDate = day + '/' + month + '/' + year;
+            document.getElementById('creation-date').textContent = formattedDate;
 
     if (username) {
         document.getElementById('user-name').textContent = username;
