ad947bcb4ce840d0a40c5b8b99ad58aa8fca97c7
João Eduardo <99341377+Crow3442@users.noreply.github.com>
2024-06-19 00:05:54 -0300

    Criação de uma página de perfil

--- codigo/assets/js/profile_page.js
+++ codigo/assets/js/profile_page.js
@@ -1,34 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Função de logout
-    document.querySelector('#sidebar ul.bottom-links li a[href$="Sair"]').addEventListener('click', function(event) {
-        event.preventDefault();
-        localStorage.removeItem('usuario'); // Remove os dados do usuário do Local Storage
-        window.location.href = '/codigo/login.html'; // Redireciona para a página de login
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+
+    sidebar.addEventListener('mouseover', () => {
+        sidebar.style.width = '250px';
     });
 
-    // Função para colapsar o sidebar
-    document.getElementById('sidebarCollapse').addEventListener('click', function() {
-        document.getElementById('sidebar').classList.toggle('collapse');
-        document.getElementById('content').classList.toggle('collapse');
+    sidebar.addEventListener('mouseout', () => {
+        sidebar.style.width = '60px';
     });
-
-    // Carregar informações do usuário
-    function carregarDadosUsuario() {
-        const usuario = JSON.parse(localStorage.getItem('usuario'));
-        if (usuario) {
-            document.querySelector('.card-body h5').textContent = usuario.nomeCompleto;
-            document.getElementById('firstName').value = usuario.nome;
-            document.getElementById('lastName').value = usuario.sobrenome;
-            document.getElementById('phoneNumber').value = usuario.telefone;
-            document.getElementById('email').value = usuario.email;
-            document.getElementById('city').value = usuario.cidade;
-            document.getElementById('state').value = usuario.estado;
-            document.getElementById('zipcode').value = usuario.cep;
-            document.getElementById('country').value = usuario.pais;
-        } else {
-            window.location.href = '/codigo/login.html'; // Redireciona para a página de login se não houver usuário
-        }
-    }
-
-    carregarDadosUsuario();
 });
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Default open tab
+document.getElementsByClassName("tab-button")[0].click();
