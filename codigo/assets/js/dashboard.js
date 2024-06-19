29e735e2ef4cd4c467ed524fd36fdb3ec13cb0c9
João Eduardo <99341377+Crow3442@users.noreply.github.com>
2024-06-18 23:57:58 -0300

    Criação de uma Dashboard

--- codigo/assets/js/dashboard.js
+++ codigo/assets/js/dashboard.js
@@ -1,26 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Função de logout
-    document.querySelector('#sidebar ul.bottom-links li a[href$="Sair"]').addEventListener('click', function(event) {
-        event.preventDefault();
-        localStorage.removeItem('usuario'); // Remove os dados do usuário do Local Storage
-        window.location.href = '/codigo/login.html'; // Redireciona para a página de login
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+
+    sidebar.addEventListener('mouseover', () => {
+        sidebar.style.width = '250px';
     });
 
-    // Função para colapsar o sidebar
-    document.getElementById('sidebarCollapse').addEventListener('click', function() {
-        document.getElementById('sidebar').classList.toggle('collapse');
-        document.getElementById('content').classList.toggle('collapse');
+    sidebar.addEventListener('mouseout', () => {
+        sidebar.style.width = '60px';
     });
-
-    // Carregar informações do usuário
-    function carregarDadosUsuario() {
-        const usuario = JSON.parse(localStorage.getItem('usuario'));
-        if (usuario) {
-            document.querySelector('main .container h1').textContent = `Bem-vindo, ${usuario.nomeCompleto}`;
-        } else {
-            window.location.href = '/codigo/login.html'; // Redireciona para a página de login se não houver usuário
-        }
-    }
-
-    carregarDadosUsuario();
 });
