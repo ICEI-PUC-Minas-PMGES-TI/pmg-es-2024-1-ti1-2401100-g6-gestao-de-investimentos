7353d84c77bedab306f2b328f80e3b6aafc15267
João Eduardo <99341377+Crow3442@users.noreply.github.com>
2024-06-27 16:25:27 -0300

    Estrutura da InvestPro Academy

--- codigo/assets/js/investpro_academy.js
+++ codigo/assets/js/investpro_academy.js
@@ -16,4 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.removeItem('loggedInUser');
         window.location.href = '/codigo/pages/login_page.html'; // Redirect to login after logout
     });
+
+
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const courseCategories = document.querySelectorAll('.course-category');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const filter = button.getAttribute('data-filter');
+
+            courseCategories.forEach(category => {
+                if (filter === 'all' || category.getAttribute('data-category') === filter) {
+                    category.style.display = 'block';
+                } else {
+                    category.style.display = 'none';
+                }
+            });
+
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+        });
+    });
+
 });
